cdea09ba45480ba0b36d22ce6eefac97458226ee
gbluma <garrett.bluma@gmail.com>
2013-03-21 13:14:13 -0500

    Added some documentation

--- main.php
+++ main.php
@@ -15,6 +15,8 @@ $deployed_tags = Exporter::deployServlets($tags, $servlets);
 if (empty($deployed_tags)) {
 	die("Fatal: No tags can be exported.\n");
 }
+
+// clean up any previously running PIDs
 shell_exec("rm -rf ./run/*");
 
 foreach($deployed_tags as $tag) {
@@ -25,14 +27,15 @@ foreach($deployed_tags as $tag) {
 		$childpid = posix_getpid();
 		$name = "EchoService-".$tag;
 		
+		// TODO: Un-hardcode this
 		require_once("deploy/$tag/services/EchoService.php");
 
-		// safe PID to file
+		// save PID to file
 		$f = fopen("./run/$name","w");
 		fwrite($f, $childpid);
 		fclose($f);
 		
-		// start service
+		// start service on it's own port
 		$r = new Reactor();
 		$r->run(new EchoService(), $childpid, "EchoService-".$tag);
 	}
@@ -41,11 +44,15 @@ foreach($deployed_tags as $tag) {
 // start proxy
 $pid = pcntl_fork();
 if ($pid == 0) {
+
+	// fire up a new listener on port 9009 to route requests
 	$r = new Reactor();
 	$r->run(new ProxyServlet(), 9009, "ProxyServlet");
 }
 
 pcntl_signal(SIGINT,'sig_handler');
+
+// wait for all subprocesses to exit before ending
 while(pcntl_waitpid(0, $status) != -1):
 	$status = pcntl_wexitstatus($status);
 	printf("Child %s exited".PHP_EOL,$status);
